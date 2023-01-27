01c636d4d7bc85fd39515c808bfce9db2b2424b1
Dhrumit1677 <dhrumit.patel1677@gmail.com>
2023-01-26 19:01:10 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+//Trigger the App component to be rendered to the DOM
+ReactDOM.render(<App />, document.getElementById('root'));
